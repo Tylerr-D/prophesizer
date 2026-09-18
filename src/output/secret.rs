f3da9ea84d72ea4d8a035d00ea738136a59d35e6
Tylerr-D <tylermishra76@gmail.com>
2026-09-18 21:10:07 +0530

    filled in secrets outputter

--- src/output/secret.rs
+++ src/output/secret.rs
@@ -1,50 +1,69 @@
 use core::time;
 use std::thread;
-use rand;
+
+// umm yea fixed the imports
 
 use super::sick::play_rick;
-use super storm::render_storm;
-use super type_out;
+use super::storm::render_storm;
+use super::type_out;
 
 pub(crate) fn output_secret(output: &String) {
     secret_found();
-    println!("{}", output);
-    todo!(
-        "[RUSTER] replace print with your outputter"
-    )
+    type_out(output);
+    // todo!(
+    //     "[RUSTER] replace print with your outputter"
+    // )
 }
 
 pub(crate) fn output_variable_secret(outputs: &Vec<String>) {
     secret_found();
-    let outputs_available = outputs.len();
+    // let outputs_available = outputs.len();
 
-    let smth: usize = rand::random_range(0..outputs_available-1);
+    // let smth: usize = rand::random_range(0..outputs_available-1);
 
-    println!("{}", outputs[smth]);
-    todo!(
-        "[RUSTER] replace print with your outputter"
-    )
+    if outputs.len() == 1 {
+        type_out(&outputs[0]);
+        println!();
+        return;
+    }
+
+    // println!("{}", outputs[smth]);
+    // todo!(
+    //     "[RUSTER] replace print with your outputter"
+    // )
+
+    let smth: usize = rand::random_range(0..outputs.len());
+    type_out(&outputs[smth]);
+    println!();
 }
 
 pub(crate) fn ascii_animation(input: String) {
     secret_found();
 
-    todo!(
-        "[RUSTER] call ascii output displayer"
-    )
+    play_rick();
+
+    // todo!(
+    //     "[RUSTER] call ascii output displayer"
+    // )
 }
 
 pub(crate) fn ascii_art(input: String) {
     secret_found();
 
-    todo!(
-        "[RUSTER] call ascii output displayer"
-    )
+    let number: u64 = input.trim().parse().unwrap_or(0);
+    render_storm(number);
+    println!();
+
+    // todo!(
+    //     "[RUSTER] call ascii output displayer"
+    // )
 }
 
 fn secret_found() {
     // [RUSTER] change print to your outputter, storm i think idk
-    println!("You have found a secret!");
+    // oke
+
+    type_out("you have found a secret");
 
     thread::sleep(time::Duration::from_secs(1));
 }
