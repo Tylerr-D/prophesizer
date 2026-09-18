e6d6634020d423bf131c6230da6cd9345397e1f5
Amaan <amaanarmaanmuhammad@gmail.com>
2026-09-18 23:27:20 +0400

    moved this to memory

--- src/main.rs
+++ src/main.rs
@@ -39,41 +39,5 @@ fn main() {
         return;
     }
 
-        
-
-        other::memory::stats();
-
-        let number = algs::process_word(&args.input);
-
-        let seen = other::memory::count_word(&args.input);
-        other::memory::record(&args.input);
-
-        println!();
-
-    println!("word: {}", args.input);
-    println!("number: {}", number);
-    output::render(number);
-
-    println!();
-
-        if seen > 0 {
-            println!("hmm, i heard this before...");
-            println!("the machine remembers \"{}\" has been fed {} time{} before",
-                     args.input, seen, if seen>1 {"s"} else {""});
-            println!();
-
-            if seen == 2 {
-                println!("gng obsession is a sin");
-            }
-
-            if seen >= 5 {
-                println!("this word lives here rent free");
-            }
-
-            if seen == 67 {
-                println!("SIX SEVENNN");
-                println!("but srsly, do you think something different will happen?")
-            }
-        }
-    }
+    other::memory::stat_generic(args);
 }
